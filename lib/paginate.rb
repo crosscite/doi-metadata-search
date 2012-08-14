c02c919e8ba440c0493b825a95e124ad1ecb3654
Karl Jonathan Ward <karl.j.ward@gmail.com>
2012-08-14 13:40:38 +0100

    Results page, pagination.

---
+++ lib/paginate.rb
@@ -0,0 +1,32 @@
+require 'will_paginate'
+
+class Paginate
+
+  def initialize page, per_page, solr_response
+    @page = page
+    @per_page = per_page
+    @response = solr_response['response']
+    @header = solr_response['header']
+  end
+
+  def docs
+    @response['docs']
+  end
+
+  def per_page
+    @per_page
+  end
+
+  def current_page
+    @page
+  end
+
+  def total_pages
+    (@response['numFound'] / per_page.to_f).ceil
+  end
+
+  def total_rows
+    @response['numFound']
+  end
+
+end
