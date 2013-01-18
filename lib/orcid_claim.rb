1e27a533bb7b777b427693fab80bae47339fa5cb
Karl Jonathan Ward <karl.j.ward@gmail.com>
2013-01-18 11:20:39 +0000

    Include more bibliographic data in orcid works, including bibtex citations.

--- lib/orcid_claim.rb
+++ lib/orcid_claim.rb
@@ -8,39 +8,20 @@ class OrcidClaim
 
   @queue = :orcid
 
-  def initialize oauth, work, record_id
+  def initialize oauth, work
     @oauth = oauth
     @work = work
-    @record_id = record_id
   end
 
-  def self.perform oauth, work, record_id
-    OrcidClaim.new(oauth, work, record_id).perform
-  end
-
-  def self.prepare orcid, doi
-    doc = {:created_at => Time.now, :orcid => orcid, :doi => doi}
-    MongoData.coll('claims').insert(doc)
-  end
-
-  def started
-    # TODO If not through reque @record_id is a BSON object instead of hash.
-    # TODO If through reque we fail later on extended chars.
-    #MongoData.coll('claims').update(@record_id, {:started_at => Time.now})
-  end
-
-  def failed e = nil
-    #MongoData.coll('claims').update(@record_id, {:failed_at => Time.now, :error => e})
-  end
-
-  def finished
-    #MongoData.coll('claims').update(@record_id, {:finished_at => Time.now})
+  def self.perform oauth, work
+    OrcidClaim.new(oauth, work).perform
   end
 
   def perform
     begin
-      started
       load_config
+
+      puts to_xml
 
       # Need to check both since @oauth may or may not have been serialized back and forth from JSON.
       uid = @oauth[:uid] || @oauth['uid']
@@ -53,15 +34,22 @@ class OrcidClaim
         post.headers['Content-Type'] = 'application/orcid+xml'
         post.body = to_xml
       end
-
-      if response.status == 200
-        finished
-      else
-        failed(response.status)
-      end
     rescue StandardError => e
       puts e
     end
+  end
+
+  def has_path? hsh, path
+    loc = hsh
+    path.each do |path_item|
+      if loc[path_item]
+        loc = loc[path_item]
+      else
+        loc = nil
+        break
+      end
+    end
+    loc != nil
   end
 
   def orcid_work_type internal_work_type
@@ -73,61 +61,140 @@ class OrcidClaim
   end
 
   def pad_date_item item
-    item_str = item.to_s.strip
-    if item_str.length < 2
-      "0" + item_str
-    else
-      item_str
+    result = nil
+    if item
+      begin
+        item_int = item.strip.to_i
+        if item_int >= 0 && item_int <= 11
+          item_str = item_int.to_s
+          if item_str.length < 2
+            result = "0" + item_str
+          elsif item_str.length == 2
+            result = item_str
+          end
+        end
+      rescue StandardError => e
+        # Ignore type conversion errors
+      end
+    end
+    result
+  end
+
+  def insert_id xml, type, value
+    xml.send(:'work-external-identifier') {
+      xml.send(:'work-external-identifier-type', type)
+      xml.send(:'work-external-identifier-id', value)
+    }
+  end
+
+  def insert_ids xml
+     xml.send(:'work-external-identifiers') {
+      insert_id(xml, 'doi', @work['doi'])
+      insert_id(xml, 'isbn', @work['proceedings']['isbn']) if has_path?(@work, ['proceedings', 'isbn'])
+      insert_id(xml, 'issn', @work['journal']['issn']) if has_path?(@work, ['journal', 'issn'])
+    }
+  end
+
+  def insert_pub_date xml
+    month_str = pad_date_item(@work['published']['month'])
+    day_str = pad_date_item(@work['published']['day'])
+    if @work['published']
+      xml.send(:'publication-date') {
+        xml.year(@work['published']['year'].to_i.to_s)
+        xml.month(month_str) if month_str
+        xml.day(day_str) if day_str
+      }
+    end
+  end
+
+  def insert_type xml
+    xml.send(:'work-type', orcid_work_type(@work['type']))
+  end
+
+  def insert_titles xml
+    subtitle = case @work['type']
+               when 'journal_article'
+                 if has_path?(@work, ['journal', 'full_title'])
+                   @work['journal']['full_title']
+                 else
+                   nil
+                 end
+               when 'conference_paper'
+                 if has_path?(@work, ['proceedings', 'title'])
+                   @work['proceedings']['title']
+                 else
+                   nil
+                 end
+               else
+                 nil
+               end
+
+    if subtitle || @work['title']
+      xml.send(:'work-title') {
+        xml.title(@work['title']) if @work['title']
+        xml.subtitle(subtitle) if subtitle
+      }
+    end
+  end
+
+  def insert_contributors xml
+    if @work['contributors'] && !@work['contributors'].count.zero?
+      xml.send(:'work-contributors') {
+        @work['contributors'].each do |contributor|
+          full_name = ""
+          full_name = contributor['given_name'] if contributor['given_name']
+          full_name += " " + contributor['surname'] if contributor['surname']
+          if !full_name.empty?
+            xml.contributor {
+              xml.send(:'credit-name', full_name)
+              # TODO Insert contributor roles and sequence once available
+              # in 'dois' mongo collection.
+              #xml.send(:'contributor-attributes') {
+              #  xml.send(:'contributor-role', 'author')
+              #}
+            }
+          end
+        end
+      }
+    end
+  end
+
+  def insert_citation xml
+    conn = Faraday.new
+    response = conn.get "http://data.crossref.org/#{@work['doi']}", {}, {
+      'Accept' => 'application/x-bibtex'
+    }
+
+    if response.status == 200
+      xml.send(:'work-citation') {
+        xml.send(:'work-citation-type', 'bibtex')
+        xml.citation {
+          xml.cdata(response.body)
+        }
+      }
     end
   end
 
   def to_xml
     root_attributes = {
       :'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-      :'xsi:schemaLocation' => 'http://www.orcid.org/ns/orcid http://orcid.github.com/ORCID-Parent/schemas/orcid-message/1.0.7/orcid-message-1.0.7.xsd',
+      :'xsi:schemaLocation' => 'http://www.orcid.org/ns/orcid http://orcid.github.com/ORCID-Parent/schemas/orcid-message/1.0.8/orcid-message-1.0.8.xsd',
       :'xmlns' => 'http://www.orcid.org/ns/orcid'
     }
 
     Nokogiri::XML::Builder.new do |xml|
       xml.send(:'orcid-message', root_attributes) {
-        xml.send(:'message-version', '1.0.7')
+        xml.send(:'message-version', '1.0.8')
         xml.send(:'orcid-profile') {
           xml.send(:'orcid-activities') {
             xml.send(:'orcid-works') {
               xml.send(:'orcid-work') {
-                xml.send(:'work-title') {
-                  xml.title(@work['title'])
-                }
-                xml.send(:'work-type', orcid_work_type(@work['type']))
-                if @work['published']
-                  xml.send(:'publication-date') {
-                    xml.year(@work['published']['year'].to_i.to_s)
-                    xml.month(pad_date_item(@work['published']['month'])) if @work['published']['month']
-                    xml.day(pad_date_item(@work['published']['day'])) if @work['published']['day']
-                  }
-                end
-                xml.send(:'work-external-identifiers') {
-                  xml.send(:'work-external-identifier') {
-                    xml.send(:'work-external-identifier-type', 'doi')
-                    xml.send(:'work-external-identifier-id', @work['doi'])
-                  }
-                }
-                xml.send(:'work-contributors') {
-                  @work['contributors'].each do |contributor|
-                    full_name = ""
-                    full_name = contributor['given_name'] if contributor['given_name']
-                    full_name += " " + contributor['surname'] if contributor['surname']
-                    if !full_name.empty?
-                      xml.contributor {
-                        xml.send(:'credit-name', full_name)
-                        # TODO ORCID is complaining about contribuor-role in attribs
-                        #xml.send(:'contributor-attributes') {
-                        #  xml.send(:'contributor-role', 'author')
-                        #}
-                      }
-                    end
-                  end
-                }
+                insert_titles(xml)
+                insert_citation(xml)
+                insert_type(xml)
+                insert_pub_date(xml)
+                insert_ids(xml)
+                insert_contributors(xml)
               }
             }
           }
