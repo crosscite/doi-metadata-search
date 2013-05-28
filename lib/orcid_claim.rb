f7d830bf5acc3285c31f70e8cdee0c20a05a3108
Karl Jonathan Ward <karl.j.ward@gmail.com>
2013-05-28 09:51:50 +0100

    Use resque-pool to manage orcid workers.

--- lib/orcid_claim.rb
+++ lib/orcid_claim.rb
@@ -23,8 +23,6 @@ class OrcidClaim
     begin
       load_config
 
-      puts to_xml
-
       # Need to check both since @oauth may or may not have been serialized back and forth from JSON.
       uid = @oauth[:uid] || @oauth['uid']
 
