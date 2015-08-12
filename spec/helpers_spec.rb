54417279850ff8ff71869341a477093545b59195
Martin Fenner <martin.fenner@datacite.org>
2015-08-12 23:50:23 +0200

    tweaks in helper specs

---
+++ spec/helpers_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe SearchResult do
+  subject { SearchResult.new(nil, nil, nil, nil) }
+
+  it 'activity' do
+    str = 'abc'
+    expect(subject.force_utf8(str)).to eq(2)
+  end
+end
--- spec/lib/helpers_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'helpers' do
-  it 'activity' do
-    str = 'abc'
-    expect(force_utf8(str)).to eq(2)
-  end
-end
