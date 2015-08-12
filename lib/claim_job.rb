770b61a0f07fdffeec1cd36a1f8336116ee4cdda
Martin Fenner <martin.fenner@datacite.org>
2015-08-12 23:33:51 +0200

    fixed small issues in refactored orcid claim job. #21

--- lib/claim_job.rb
+++ lib/claim_job.rb
@@ -7,9 +7,9 @@ class ClaimJob
   def perform(session_info, work)
     oauth_expired = false
 
-    claim = Claim.new(work).to_xml
+    orcid_claim = OrcidClaim.new(work).to_xml
     orcid_client = OrcidClient.new(session_info)
-    response = orcid_client.post(claim)
+    response = orcid_client.post(orcid_claim)
     oauth_expired = response.status >= 400
 
     !oauth_expired
