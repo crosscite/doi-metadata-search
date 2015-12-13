322d7a1a2a283b9835d8c613c1647edc93f483d7
Martin Fenner <martin.fenner@datacite.org>
2015-12-13 14:18:04 +0100

    don't precompile assets

--- vendor/cookbooks/doi_metadata_search/recipes/default.rb
+++ vendor/cookbooks/doi_metadata_search/recipes/default.rb
@@ -23,5 +23,5 @@ capistrano node["application"] do
   user            ENV['DEPLOY_USER']
   group           ENV['DEPLOY_GROUP']
   rails_env       ENV['RAILS_ENV']
-  action          [:config, :bundle_install, :npm_install, :consul_install, :precompile_assets, :whenever, :restart]
+  action          [:config, :bundle_install, :npm_install, :consul_install, :whenever, :restart]
 end
