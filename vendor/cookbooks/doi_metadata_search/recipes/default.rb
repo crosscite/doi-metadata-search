cf77b14c5f846245f55f0a5501301955a1c396c5
Martin Fenner <martin.fenner@datacite.org>
2015-12-15 22:28:48 +0100

    configure rsyslog

--- vendor/cookbooks/doi_metadata_search/recipes/default.rb
+++ vendor/cookbooks/doi_metadata_search/recipes/default.rb
@@ -23,5 +23,5 @@ capistrano node["application"] do
   user            ENV['DEPLOY_USER']
   group           ENV['DEPLOY_GROUP']
   rails_env       ENV['RAILS_ENV']
-  action          [:config, :bundle_install, :npm_install, :consul_install, :whenever, :restart]
+  action          [:config, :bundle_install, :npm_install, :consul_install, :rsyslog_config, :whenever, :restart]
 end
