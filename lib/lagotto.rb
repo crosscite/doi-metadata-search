2c2a9f472e31e75fb03de5c0c5f8186efff140b6
Martin Fenner <martin.fenner@datacite.org>
2016-04-06 15:35:48 +0200

    rewrote lagotto data fetching

--- lib/lagotto.rb
+++ lib/lagotto.rb
@@ -7,39 +7,36 @@ module Sinatra
     SOURCES = {
       "bmc_fulltext" => "BioMed Central",
       "citeulike" => "CiteULike",
-      "datacite_related" => "DataCite",
-      "datacite_github" => "DataCite",
-      "europe_pmc_fulltext" => "Europe PMC",
+      "datacite_related" => "DataCite (RelatedIdentifier)",
+      "datacite_github" => "DataCite (Github)",
+      "europe_pmc_fulltext" => "Europe PMC (Fulltext)",
       "orcid" => "ORCID",
-      "nature_opensearch" => "Nature OpenSearch",
-      "plos_fulltext" => "PLOS",
+      "nature_opensearch" => "Nature (OpenSearch)",
+      "plos_fulltext" => "PLOS (Fulltext)",
       "wikipedia" => "Wikipedia"
     }
 
-    # query Link Store server, fetch references, discard information coming from DataCite
+    # query Event Data server, fetch relations
     def get_relations(dois)
       return {} unless dois.present? && ENV["LAGOTTO_URL"].present?
 
-      response = Maremma.post "#{ENV['LAGOTTO_URL']}/api/relations", content_type: "text/html", data: work_ids_as_string(dois)
+      response = Maremma.get "#{ENV['LAGOTTO_URL']}/api/works?#{ids_as_string(dois)}&type=doi"
       response = {} if response["errors"].present?
 
-      response.fetch("data", {}).fetch("relations", []).map do |reference|
-        source = reference.fetch("source_id", nil)
-        source = SOURCES.fetch(source, source)
+      response.fetch("data", {}).fetch("works", []).map do |reference|
+        signposts = reference.fetch("events", {}).map do |k, v|
+          { "title" => SOURCES.fetch(k, k),
+            "count" => v,
+            "name" => k }
+        end
 
-        { doi: reference.fetch("work_id", "")[15..-1],
-          id: reference.fetch("id", ""),
-          relation: reference.fetch("relation_type_id", "references").camelize,
-          source: source,
-          title: reference.fetch("title", nil),
-          container_title: reference.fetch("container-title", nil),
-          author: reference.fetch("author", nil),
-          issued: reference.fetch("issued", nil) }
-      end.uniq.select { |item| item[:source] !~ /orcid.*/ }.group_by { |item| item[:doi] }
+        { doi: reference.fetch("DOI", nil),
+          signposts: signposts }
+      end.group_by { |item| item[:doi] }
     end
 
-    def work_ids_as_string(dois)
-      "work_ids=" + dois.map { |doi| "http://doi.org/#{doi}" }.join(",")
+    def ids_as_string(dois)
+      "ids=" + dois.join(",")
     end
   end
 
--- spec/lagotto_spec.rb
+++ spec/lagotto_spec.rb
@@ -7,20 +7,20 @@ describe "Lagotto", type: :model, vcr: true do
   let(:claimed) { false }
   let(:related_identifiers) { {} }
 
-  let(:doi) { "10.5517/CC1JZZ2K" }
-  let(:dois) { ["10.1594/PANGAEA.845725","10.5517/CC1JZZ2K"] }
+  let(:doi) { "10.17600/14000300" }
+  let(:dois) { ["10.15468/DIPJCR","10.17600/14000300"] }
 
   subject { SearchResult.new(solr_doc, solr_result, claimed, related_identifiers) }
 
   context "work_ids_as_string" do
     it "get single doi" do
-      response = subject.work_ids_as_string([doi])
-      expect(response).to eq("work_ids=http://doi.org/10.5517/CC1JZZ2K")
+      response = subject.ids_as_string([doi])
+      expect(response).to eq("ids=10.17600/14000300")
     end
 
     it "get multiple dois" do
-      response = subject.work_ids_as_string(dois)
-      expect(response).to eq("work_ids=http://doi.org/10.1594/PANGAEA.845725,http://doi.org/10.5517/CC1JZZ2K")
+      response = subject.ids_as_string(dois)
+      expect(response).to eq("ids=10.15468/DIPJCR,10.17600/14000300")
     end
   end
 
@@ -29,28 +29,18 @@ describe "Lagotto", type: :model, vcr: true do
       response = subject.get_relations([doi])
       relations = response.fetch(doi.upcase, [])
       expect(relations.length).to eq(1)
-      expect(relations.first).to eq(doi: "10.5517/CC1JZZ2K",
-                                    id: "http://doi.org/10.1016/j.ica.2016.02.005",
-                                    relation: "IsSupplementTo",
-                                    source: "DataCite",
-                                    title: "A dinuclear iron(II) complex bearing multidentate pyridinyl ligand: Synthesis, characterization and its catalysis on the hydroxylation of aromatic compounds",
-                                    container_title: nil,
-                                    author: [],
-                                    issued: {"date-parts"=>[[2016, 2, 14]]})
+      expect(relations.first).to eq(:doi=>"10.17600/14000300", :signposts=>[{"title"=>"DataCite (RelatedIdentifier)",
+                                                                            "count"=>1,
+                                                                            "name"=>"datacite_related"}])
     end
 
     it "get multiple dois" do
       response = subject.get_relations(dois)
-      expect(response.keys).to eq(["10.5517/CC1JZZ2K", "10.1594/PANGAEA.845725"])
+      expect(response.keys).to eq(["10.15468/DIPJCR", "10.17600/14000300"])
       relations = response.fetch(dois.first.upcase, [])
-      expect(relations.first).to eq(doi: "10.1594/PANGAEA.845725",
-                                    id: "http://doi.org/10.5194/essd-2015-38",
-                                    relation: "IsSupplementTo",
-                                    source: "DataCite",
-                                    title: "Observational gridded runoff estimates for Europe (E-RUN version 1.0)",
-                                    container_title: nil,
-                                    author: [],
-                                    issued: {"date-parts"=>[[2016, 1, 18]]})
+      expect(relations.first).to eq(:doi=>"10.15468/DIPJCR", :signposts=>[{"title"=>"DataCite (RelatedIdentifier)",
+                                                                           "count"=>1002,
+                                                                           "name"=>"datacite_related"}])
     end
 
     it "get no doi" do
