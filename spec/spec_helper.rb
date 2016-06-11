3d74d186cf838beaf55110a790d90906cc4c9fab
Martin Fenner <martin.fenner@datacite.org>
2016-06-11 11:49:55 +0200

    use different env variable name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,7 +50,7 @@ RSpec.configure do |config|
       uid: '0000-0003-1419-2405',
       info: { 'role' => "admin",
               'name' => "Josiah Carberry",
-              'api_key' => ENV['VOLPINO_API_KEY'] },
+              'api_key' => ENV['ORCID_UPDATE_TOKEN'] },
       extra: {},
       credentials: { 'expires' => nil,
                      'expires_at' => Time.now + 1.year }
@@ -88,7 +88,7 @@ VCR.configure do |c|
   c.ignore_localhost = true
   c.ignore_hosts 'codeclimate.com'
   c.filter_sensitive_data('<API_KEY>') { ENV['API_KEY'] }
-  c.filter_sensitive_data('<VOLPINO_API_KEY>') { ENV['VOLPINO_API_KEY'] }
+  c.filter_sensitive_data('<ORCID_UPDATE_TOKEN>') { ENV['ORCID_UPDATE_TOKEN'] }
   c.allow_http_connections_when_no_cassette = false
   c.configure_rspec_metadata!
 end
