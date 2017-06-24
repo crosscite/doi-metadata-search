2497b6fcbaf4a3221d99f49d1b93193fc21e2be2
Martin Fenner <martin.fenner@datacite.org>
2017-06-24 15:50:51 +0200

    use asymmetric keys for jwt token. datacite/volpino#41

--- lib/session_helper.rb
+++ lib/session_helper.rb
@@ -2,8 +2,13 @@ require 'sinatra/base'
 
 module Sinatra
   module SessionHelper
+    # replace newline characters with actual newlines
+    def public_key
+      OpenSSL::PKey::RSA.new(ENV['JWT_PUBLIC_KEY'].to_s.gsub('\n', "\n"))
+    end
+
     def current_user
-      @current_user ||= cookies[:jwt].present? ? User.new((JWT.decode cookies[:jwt], ENV['JWT_SECRET_KEY']).first) : nil
+      @current_user ||= cookies[:_datacite_jwt].present? ? User.new((JWT.decode cookies[:_datacite_jwt], public_key, true, { :algorithm => 'RS256' }).first) : nil
     end
 
     def user_signed_in?
