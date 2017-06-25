cc9101e24740aa56c2db71a4fbedb828e8b4c26f
Martin Fenner <martin.fenner@datacite.org>
2017-06-25 14:00:37 +0200

    check for expired jwt token. datacite/datacite#158

--- lib/user.rb
+++ lib/user.rb
@@ -4,8 +4,14 @@ class User
   attr_accessor :name, :uid, :email, :jwt, :role, :orcid
 
   def initialize(jwt)
+    return false unless jwt.present?
+
+    # decode token using SHA-256 hash algorithm
     public_key = OpenSSL::PKey::RSA.new(ENV['JWT_PUBLIC_KEY'].to_s.gsub('\n', "\n"))
     jwt_hsh = JWT.decode(jwt, public_key, true, { :algorithm => 'RS256' }).first
+
+    # check whether token has expired
+    return false unless Time.now.to_i < jwt_hsh["exp"]
 
     @jwt = jwt
     @uid = jwt_hsh.fetch("uid", nil)
@@ -13,7 +19,7 @@ class User
     @email = jwt_hsh.fetch("email", nil)
     @role = jwt_hsh.fetch("role", nil)
   end
-
+  
   alias_method :orcid, :uid
 
   # Helper method to check for admin user
