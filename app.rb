a05c7d2ca45029c73b08ebb0f8ac98f9b2c83c74
Martin Fenner <mfenner@datacite.org>
2017-10-22 08:44:12 +0200

    updated version

--- app.rb
+++ app.rb
@@ -24,6 +24,7 @@ ENV['LOG_LEVEL'] ||= "info"
 ENV['RA'] ||= "datacite"
 ENV['TRUSTED_IP'] ||= "172.0.0.0/8"
 ENV['API_URL'] ||= "https://api.datacite.org"
+ENV['ORCID_UPDATE_URL'] ||= "https://profiles.datacite.org"
 ENV['DATA_URL'] ||= "https://data.datacite.org"
 ENV['CDN_URL'] ||= "https://assets.datacite.org"
 
