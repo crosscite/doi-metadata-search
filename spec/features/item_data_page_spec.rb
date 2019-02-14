34ec3f4721419dc17612b5943a0b9e712e16b0bb
Richard Hallett <richard@richardhallett.com>
2019-02-14 17:29:55 +0100

    Basics coverage of metrics

--- spec/features/item_data_page_spec.rb
+++ spec/features/item_data_page_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper'
 describe 'item', type: :feature, js: true, vcr: true do
   describe 'metrics tests' do
 
-    before do
+    it 'when metrics should exist has metrics' do
       visit '/works/10.7272/q6g15xs4'
-    end
-
-    it 'has metrics views' do
       expect(page).to have_css 'span.metrics-views'
-    end
-
-    it 'has metrics downloads' do
       expect(page).to have_css 'span.metrics-downloads'
     end
+
+    it 'when metrics should not exist doesnt have has metrics' do
+      visit '/works/10.15468/dl.ow2im1'
+      expect(page).not_to have_css 'span.metrics-views'
+      expect(page).not_to have_css 'span.metrics-downloads'
+    end
+
   end
 end
