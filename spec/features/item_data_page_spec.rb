fc0f839dbd7a0fe3ba6e4ac88e6fb69e36f1b910
Kristian Garza <kj.garza@gmail.com>
2019-02-20 16:29:28 +0100

    fixed test

--- spec/features/item_data_page_spec.rb
+++ spec/features/item_data_page_spec.rb
@@ -6,16 +6,16 @@ describe 'item', type: :feature, js: true, vcr: true do
     it 'when usage metrics should be displayed' do
       visit '/works/10.7272/q6g15xs4'
       # Check for usage data spans
-      expect(page).to have_css 'span.usage-views'
-      expect(page).to have_css 'span.usage-downloads'
+      expect(page).to have_css 'a.usage-counts.usage-views'
+      expect(page).to have_css 'a.usage-counts.usage-downloads'
       # Check for the charts
       expect(page).to have_css 'div.usage-charts'
     end
 
     it 'when usage metrics should be hidden' do
       visit '/works/10.21956/aasopenres.13896.r26067'
-      expect(page).not_to have_css 'span.usage-views'
-      expect(page).not_to have_css 'span.usage-downloads'
+      expect(page).not_to have_css 'a.usage-counts.usage-views'
+      expect(page).not_to have_css 'a.usage-counts.usage-downloads'
     end
 
   end
