cf7221cc225376ae22f6c637ab37e631c763b287
Kristian Garza <kj.garza@gmail.com>
2019-02-21 12:46:10 +0100

    added usersnap

---
+++ public/javascripts/usersnap.js
@@ -0,0 +1,2 @@
+(function() { var s = document.createElement("script"); s.type = "text/javascript"; s.async = true; s.src = '//api.usersnap.com/load/975c8c52-fa7e-4374-94eb-d7822dfc0977.js';
+var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); })();
