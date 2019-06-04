43b9185d8a98266572836a806284310b5f2653cf
Kristian Garza <kj.garza@gmail.com>
2019-06-04 16:26:25 +0200

    request metdata for citations from datacite

--- lib/api.rb
+++ lib/api.rb
@@ -97,22 +97,47 @@ module Sinatra
       if params.fetch(:id, nil).present?
         url = "#{ENV['API_URL']}/events/#{params.fetch(:id)}"
       else
-        params = { id: params.fetch(:id, nil),
-                   'source-id' => params.fetch('source-id', nil),
-                   'subj-id'   => params.fetch('subj-id', nil),
-                   'relationTypeId'   => params.fetch(relations.join(', '), nil),
-                   'page[size]'=> params.fetch('page[size]', nil),
-                   'obj-id'    => params.fetch('obj-id', nil),
-                   'occurredAt'=> params.fetch('occurred_in', nil), 
-                   'extra' => true,
-                   query: params.fetch(:query, nil) }.compact
+        params =
+        {
+          id: params.fetch(:id, nil),
+          'source-id'        => params.fetch('source-id', nil),
+          'subj-id'          => params.fetch('subj-id', nil),
+          'relationTypeId'   => params.fetch(relations.join(', '), nil),
+          'page[size]'       => params.fetch('page[size]', nil),
+          'obj-id'           => params.fetch('obj-id', nil),
+          'doi'              => params.fetch('doi', nil),
+          'occurredAt'       => params.fetch('occurred_in', nil), 
+          'include'          => params.fetch('include', nil), 
+          'extra'            => true,
+          query: params.fetch(:query, nil)
+        }.compact
         url = "#{ENV['API_URL']}/events?" + URI.encode_www_form(params)
       end
-  
       response = params[:response].present? ? params[:response] : Maremma.get(url, headers: {"Accept"=> "application/vnd.api+json; version=2"}, timeout: TIMEOUT)
       { data: response.body.fetch("data", []),
+        included: response.body.fetch("included", []),
         errors: Array(response.body.fetch("errors", [])),
         meta: response.body.fetch("meta", {}) }
+    end
+
+    def get_citations_metadata(events, params = {})
+
+      dois = events.fetch(:included,[]).map do |citation| 
+        doi = citation.fetch('id',nil)
+        doi = Array(/\A(?:(http|https):\/(\/)?(dx\.)?(doi.org|handle.test.datacite.org)\/)?(doi:)?(10\.\d{4,5}\/.+)\z/.match(doi)).last
+        doi = doi.delete("\u200B").downcase if doi.present?
+        doi if doi.present?
+      end
+  
+      url = "#{ENV['API_URL']}/dois?ids=#{dois.join(",")}&" + URI.encode_www_form({"page[size]"=> 50})
+      # dependency injection
+      response = params[:response].present? ? params[:response] : Maremma.get(url, timeout: 20)
+
+      {
+        data: Array(response.body.fetch("data", [])),
+        errors: Array(response.body.fetch("errors", [])),
+        meta: response.body.fetch("meta", {}) 
+      }
     end
   
   end
