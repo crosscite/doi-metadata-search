6ec130a4e220d1e3a504abafe2c8fc2712132a02
kjgarza <kgarza@datacite.org>
2020-03-18 11:06:30 +0100

    enable gzip compressing for faster response

--- lib/api.rb
+++ lib/api.rb
@@ -31,7 +31,7 @@ module Sinatra
 
         url = "#{ENV['API_URL']}/dois?" + URI.encode_www_form(params)
       end
-      response = Maremma.get(url, headers: { "Content-Type" => "application/json;charset=UTF-8" }, timeout: TIMEOUT)
+      response = Maremma.get(url, headers: { "Content-Type" => "application/json;charset=UTF-8", "Accept-Encoding"=> "gzip" }, timeout: TIMEOUT)
 
       { data: response.body.fetch("data", []),
         included: response.body.fetch("included", []),
